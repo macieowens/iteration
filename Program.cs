0f9a88721fb323617de01bc76b96d11a6743b579
macieowens <112588931+macieowens@users.noreply.github.com>
2022-11-17 19:50:06 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var numbers = new List<int>();
+
+            var myNumber = 0;
+
+            do
+            {
+                myNumber++; // 2
+
+                numbers.Add(myNumber); // 1, 2, 3, 4, 100
+
+            } while (myNumber < 100);
+
+            while (myNumber < 200)
+            {
+                myNumber++;
+
+                numbers.Add(myNumber);
+            }
+
+            Console.WriteLine("Increase");
+
+            foreach(var item in numbers)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Decrease");
+
+            for(int i = 199; i <= numbers.Count && i >= 0; i--)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+        }
+    }
+}
